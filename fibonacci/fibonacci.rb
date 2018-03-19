5fb80e2af5fd03fd69fb86d58338707eff99e15c
howardbdev <howarddevennish@gmail.com>
2018-03-19 08:04:58 -0400

    set up solution file for deployment

---
+++ fibonacci/fibonacci.rb
@@ -0,0 +1,10 @@
+require 'pry-byebug'
+# Feel free to create helpers or structure however you'd like.
+# Pry is included in case you'd like to run this file and test it,
+# but you may remove it if you like.
+
+def fibonacci(n)
+  # your code here :)
+end
+
+Pry.start
