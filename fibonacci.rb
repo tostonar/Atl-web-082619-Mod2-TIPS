1c2fca8a819705420fdd7db820e8f09b65f3ebec
Andrea T <tostonar@gmail.com>
2019-09-23 15:05:22 -0400

    Not sure

--- fibonacci.rb
+++ fibonacci.rb
@@ -4,6 +4,24 @@ require 'pry-byebug'
 
 def fibonacci(n)
   # your code here :)
+  if n < 0
+    return "You must provide a number greater than 0"
+  end
+  f = []
+
+  if n == 1 
+    f = [1] 
+  elsif n == 2
+    f = [1,1]
+  else
+    f = [1,1]
+    f << (f[-1] + f[-2]) 
+    n -= 1
+    fibonacci(n)
+  end
+  f[-1]
 end
 
-Pry.start
+puts fibonacci(7)
+
+# Pry.start
